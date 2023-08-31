ddbe171229d08a234d3d8fe5d59476b69e6c9a18
Harsh Patel <126771343+harshkumarpatelh@users.noreply.github.com>
2023-08-31 14:25:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import joblib as jb
 def main(model,cv):
     st.title('Spam Filter'.upper())
     st.markdown("<h1 style = 'text-align : center; font-size: 25px; color: blue ;'></h1>",unsafe_allow_html=True)
+    st.markdown("hide_github_icon",unsafe_allow_html=True)
     result =''
 
     text_message = st.text_input('Enter your text message')
